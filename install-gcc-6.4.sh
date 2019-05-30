ed7a0f4d2fa2b5fad929de624318d86206c90b7a
abhpc <37471375+abhpc@users.noreply.github.com>
2019-05-30 22:01:53 +0800

    Update install-gcc-6.4.sh

--- install-gcc-6.4.sh
+++ install-gcc-6.4.sh
@@ -5,21 +5,21 @@ INSTALL_DIR="/opt"
 JN="10"
 
 # Install gmp 6.1.2
-tar xvf gmp-6.1.2.tar.xz 
+tar xvf gmp-6.1.2.tar.xz
 cd gmp-6.1.2
 ./configure --prefix=$INSTALL_DIR/gmp-6.1.2
 make -j $JN && make install
 cd ..
 
 # Install mpfr 3.1.6
-tar xvf mpfr-3.1.6.tar.gz 
+tar xvf mpfr-3.1.6.tar.gz
 cd mpfr-3.1.6
 ./configure --prefix=$INSTALL_DIR/mpfr-3.1.6 --with-gmp=$INSTALL_DIR/gmp-6.1.2
 make -j $JN && make install
 cd ..
 
 # Install mpc 1.0.3
-tar xvf mpc-1.0.3.tar.gz 
+tar xvf mpc-1.0.3.tar.gz
 cd mpc-1.0.3
 ./configure --prefix=$INSTALL_DIR/mpc-1.0.3 --with-gmp=$INSTALL_DIR/gmp-6.1.2 --with-mpfr=$INSTALL_DIR/mpfr-3.1.6
 make -j $JN && make install
@@ -32,7 +32,6 @@ echo "$INSTALL_DIR/mpc-1.0.3/lib"  >> /etc/ld.so.conf
 ldconfig -v
 
 # Install GCC 6.4.0
-wget https://ftp.gnu.org/pub/gnu/gcc/gcc-6.4.0/gcc-6.4.0.tar.xz
 tar xvf gcc-6.4.0.tar.xz
 cd gcc-6.4.0
 ./configure --enable-checking=release --enable-languages=c,c++ --disable-multilib \
